9e6c2ce96b68a94823ad4f1b17aeb9a4bbfcefea
Tarun Agarwal <sudotarun@gmail.com>
2020-01-06 15:57:15 +0530

    updated

--- Library.py
+++ Library.py
@@ -1,3 +1,20 @@
+import sqlite3
+
+books = sqlite3.connect('books.db')
+user = sqlite3.connect('users.db')
+
+b = books.cursor()
+u = user.cursor()
+
+
+def create():
+    b.execute('CREATE TABLE IF NOT EXISTS Books(Title TEXT, Author TEXT, Year INTEGER)')
+    u.execute('CREATE TABLE IF NOT EXISTS Users(Name TEXT, Key BLOB, Uid BLOB)')
+
+
+create()
+
+
 class Book:
 
     def __init__(self, title, author, year):
@@ -5,6 +22,8 @@ class Book:
         self.author = author
         self.year = year
         self.next = None
+        b.execute("INSERT INTO Books(Title, Author, Year) VALUES(?, ?, ?)", (title, author, year))
+        books.commit()
 
 
 class User:
@@ -15,6 +34,8 @@ class User:
         self.uid = uid
         self.books = []
         self.next = None
+        u.execute("INSERT INTO Users(Name, Key, Uid) VALUES(?, ?, ?)", (name, key, uid))
+        user.commit()
 
 
 # class Admin:
@@ -45,7 +66,7 @@ class Library:
     def login(self, uid, key):
         if uid == self.user.uid:
             if key == self.user.key:
-                pass
+                return
             else:
                 print("Wrong Password")
                 return False
@@ -58,7 +79,7 @@ class Library:
                 else:
                     if uid == prev.next.log.uid:
                         if key == prev.next.key:
-                            pass
+                            return
                         else:
                             print("Wrong Password")
                             return False
@@ -110,11 +131,13 @@ class Library:
                 break
             if id == c.uid:
                 b = c.books
-                print("|    {}  |   {}  |   {}  |".format(c.name, c.uid, b))
+                print("Name:"+c.name, "UID:"+c.uid)
+                print("Books:", end=" ")
+                for a in(b):
+                    print(a,"|", end=" ")
             c = c.next
 
     def displayBook(self):
-        print("|==TITLE==|==AUTHOR==|==YEAR==|")
         if self.head is None:
             print("Empty!")
             return
@@ -122,61 +145,76 @@ class Library:
         while True:
             if c is None:
                 break
-            print("{}|{}|{}|".format(c.title, c.author, c.year))
+            print("|  {} | {} | {}".format(c.title, c.author, c.year))
+            print("|-------------------------------------|")
             c = c.next
 
 
-lib = Library()
-t = ["ALCHEMIST", "5 AM", "RICH DAD POOR DAD", "THINK", "UNKNOWN"]
-a = ["ROBIN", "ROBIN SHARMA", "ROBERT", "NAPOLIAN", "ANNONYMOUS"]
-y = ["2001", "2012", "2015", "1995", "1999"]
-for n in range(len(t)):
-    lib.insert(Book(t[n],a[n],y[n]))
+if __name__ == '__main__':
 
-while True:
-    print("|========LIBRARY========|")
-    print("Menu:n for new user, l for login")
-    u = input("")
-    if u == "n":
-        a = input("name:")
-        b = input("uid:")
-        c = input("key:")
+    lib = Library()
+    # t = ["ALCHEMIST", "5 AM", "RICH DAD POOR DAD", "THINK", "UNKNOWN"]
+    # a = ["ROBIN", "ROBIN SHARMA", "ROBERT", "NAPOLIAN", "ANNONYMOUS"]
+    # y = ["2001", "2012", "2015", "1995", "1999"]
+    # for n in range(len(t)):
+    #     lib.insert(Book(t[n], a[n], y[n]))
+
+    def log_menu():
+        print("|========LIBRARY========|")
+        print("|  n - new user")
+        print("|  l - login")
+        print("|-----------------------|")
+        u = input("|  Enter:")
+        if u == "n":
+            new()
+        if u == "l":
+            login()
+
+    def login():
+        print("|----------Login---------|")
+        uid = input("|  uid:")
+        key = input("|  key:")
+        if lib.login(uid, key) is False:
+            login()
+        return
+
+    def new():
+        print("|-------REGISTER--------|")
+        a = input("|  name:")
+        b = input("|  uid:")
+        c = input("|  key:")
         lib.register(User(a, b, c))
-        print("Login")
-        b = input("uid:")
-        c = input("key:")
-        if lib.login(b, c) is True:
-            break
-    if u == "l":
-        b = input("uid:")
-        c = input("key:")
-        if lib.login(b, c) is True:
-            break
-    break
+        login()
 
-while True:
-    print("|========LIBRARY========|")
-    print("|  1-display books")
-    print("|  2-add books")
-    print("|  3-issue")
-    print("|  0-exit")
-    a = int(input("|  Enter:"))
-    print("|=======================|")
-    if a == 1:
-        print("====BOOKS====")
-        lib.displayBook()
-    if a == 2:
-        a = input("title:")
-        b = input("author:")
-        c = int(input("year:"))
-        lib.insert(Book(a, b, c))
-        print("|============BOOKS============|")
-        lib.displayBook()
-        print("|=========================|")
-    if a == 3:
-        n = input("Enter title:")
-        u = input("UID")
-        lib.issue(n, u)
-        lib.displayUser(u)
-    if a == 0:
-        break
+    def book_menu():
+        print("|========LIBRARY========|")
+        print("|  1 - display books")
+        print("|  2 - add books")
+        print("|  3 - issue")
+        print("|  0 - exit")
+        a = int(input("|  Enter:"))
+        #print("|=======================|")
+        if a == 1:
+            print("|----------------BOOKS----------------|")
+            #print("|----------------------------------|n")
+            lib.displayBook()
+        if a == 2:
+            d = input("title:")
+            b = input("author:")
+            c = int(input("year:"))
+            lib.insert(Book(d, b, c))
+            print("|============BOOKS============|")
+            lib.displayBook()
+            print("|=========================|")
+        if a == 3:
+            n = input("Enter title:")
+            u = input("UID:")
+            lib.issue(n, u)
+            lib.displayUser(u)
+            print("")
+        if a == 0:
+            exit()
+
+    log_menu()
+    while True:
+        book_menu()
